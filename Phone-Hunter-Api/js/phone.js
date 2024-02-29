b1db14ceed9666f4701f44aea2f2317046c4533e
Zioun <jionkhan0@gmail.com>
2024-02-29 10:54:11 +0600

    added

--- Phone-Hunter-Api/js/phone.js
+++ Phone-Hunter-Api/js/phone.js
@@ -1,24 +1,26 @@
-const loadPhone = async (searchValue) => {
+const loadPhone = async (searchValue, isShowAll) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchValue}`)
     const data = await res.json()
     const phones = data.data
-    displayPhones(phones);
+    displayPhones(phones, isShowAll);
 }
 
-const displayPhones = phones => {
+const displayPhones = (phones, isShowAll) => {
     const cardArea = document.getElementById("card-area");
     // btn show and hide
     const showBtn = document.getElementById("show-all");
-    if(phones.length > 12){
+    if(phones.length > 12 && !isShowAll){
         showBtn.classList.remove("hidden")
     }else{
         showBtn.classList.add("hidden");
     }
     // slich
-    const slicePhn = phones.slice(0,12);
+    if(!isShowAll){
+        phones = phones.slice(0,12);
+    }
     // clear data 
     cardArea.textContent = "";
-    slicePhn.forEach(phone => {
+    phones.forEach(phone => {
         const div = document.createElement("div");
         div.classList.add("card", "card-compact", "w-96", "bg-base-100", "shadow-xl", "border", "pt-5");
         div.innerHTML = `
@@ -26,20 +28,38 @@ const displayPhones = phones => {
             <div class="card-body">
                 <h2 class="card-title">${phone.phone_name}</h2>
                 <p>${phone.slug}</p>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Buy Now</button>
+                <div class="card-actions justify-center">
+                    <button class="btn btn-primary">Show Details</button>
                 </div>
             </div>
         `;
         cardArea.appendChild(div);
     });
+    preLoader(false);
 }
 
-const searchBtn = () =>{
+// search button
+const searchBtn = (isShowAll) =>{
     const searchValue = document.getElementById("search_input").value;
-    loadPhone(searchValue);
+    loadPhone(searchValue ,isShowAll);
+    preLoader(true);
 }
 
 
+// preloader
 
+const preLoader = (loaderIcon) =>{
+    const preLoaderIcon = document.getElementById("pre-loder");
+    if(loaderIcon){
+        preLoaderIcon.classList.remove("hidden");
+    }else{
+        preLoaderIcon.classList.add("hidden");
+    }
+}
+
+// show all 
+
+const showAll = () => {
+    searchBtn(true);
+}
 
